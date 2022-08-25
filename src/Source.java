f03fd83747f384e2ad5bd2dfd9e9fe31a8dd9854
AliaksandrKryvapust <alexandrkryvopust@gmail.com>
2022-08-25 10:55:43 +0200

    Final

---
+++ src/Source.java
@@ -0,0 +1,18 @@
+import java.util.Objects;
+
+public class Source {
+    public static void main(String[] args) {
+        String str ="red";
+        System.out.println(updateLight(str).equals("green"));
+    }
+    public static String updateLight(String current) {
+        String [] arr = {"green", "yellow", "red"};
+        for (int i = 0; i < arr.length; i++) {
+            if (Objects.equals(current, arr[arr.length - 1])) { return arr[0];}
+            else if (Objects.equals(current, arr[i])) {
+                return arr[i+1];
+            }
+        }
+        return "";
+    }
+}
